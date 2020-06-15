b6fc07266e83fc8e4c0f5cb12f779cedef5a23fd
waiyan13 <zaw.waiyan.lwin96@gmail.com>
2020-06-15 14:01:21 +0630

    Add generated pelican quickstart files

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Zaw W. Lwin'
+SITENAME = 'DevBlog 13'
+SITEURL = 'https://waiyan13.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Yangon'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = ()
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://linkedin.com/in/waiyan13'),
+          ('Github', 'https://github.com/waiyan13'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
